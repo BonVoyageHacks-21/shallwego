22fdb176435f386385d28e4d6df5267770514ac0
Hua Lun <45708294+hua-lun@users.noreply.github.com>
2021-07-18 17:25:21 +0800

    Add files via upload

---
+++ ver1/Code.js
@@ -0,0 +1,50 @@
+/* What should the add-on do after it is installed */
+function onInstall() {
+    onOpen();
+}
+
+/* What should the add-on do when a document is opened */
+function onOpen() {
+    DocumentApp.getUi()
+        .createAddonMenu() // Add a new option in the Google Docs Add-ons Menu
+        .addItem("Google Maps", "showSidebar")
+        .addToUi();  // Run the showSidebar function when someone clicks the menu
+}
+
+/* Show a 300px sidebar with the HTML from googlemaps.html */
+function showSidebar() {
+    var html = HtmlService.createTemplateFromFile("information")
+        .evaluate()
+        .setTitle("information"); // The title shows in the sidebar
+    DocumentApp.getUi().showSidebar(html);
+}
+
+/* This Google Script function does all the magic. */
+function insertGoogleMap(e) {
+    var map = Maps.newStaticMap()
+        .setSize(600, 400) // Insert a Google Map 800x600 px
+        .setMarkerStyle(Maps.StaticMap.MarkerSize.MID, Maps.StaticMap.Color.RED, 'T')
+        .addMarker(e)
+        .setZoom(15)
+        .setCenter(e); // e contains the address entered by the user
+
+    DocumentApp.getActiveDocument()
+        .getCursor() // Find the location of the cursor in the document
+        .insertInlineImage(map.getBlob()); // insert the image at the cursor
+}
+
+/* Add text information to document. */
+function appendDestinationInformation(information) {
+    var body = DocumentApp.getActiveDocument().getBody();
+
+    // append to current document the paragraph "Countries to visit"
+    body.appendParagraph(information);
+
+}
+
+/* Goes to next page */
+function showNext(page) {
+    var ui = HtmlService.createHtmlOutputFromFile(page)
+        .setTitle(page);
+    DocumentApp.getUi().showSidebar(ui);
+}
